0f6f8dd4c23cd2e4ff59dd21067bdd8f5d002fbf
Vitaly Rudenko <maurice.dederick@gmail.com>
2021-03-19 21:17:19 +0200

    Completed PatternBuilder implementation and tests

--- app/PatternBuilder.js
+++ app/PatternBuilder.js
@@ -1,35 +1,98 @@
 class PatternBuilder {
     build(input) {
-        input = input.trim().toLowerCase();
+        input = input.toLowerCase();
+
+        const result = [];
 
         let currentType = 'text';
         let value = '';
+        let nested = 0;
 
         let i = 0;
         while (i <= input.length) {
-            const previousCharacter = i > 0 ? input[i - 1] : null;
             const character = i < input.length ? input[i] : null;
 
-            let type = 'text';
-
+            let type = currentType;
             let skip = false;
 
-
-            if (previousCharacter === '#' && character !== '#') {
-                type = 'variable';
-                skip = true;
+            if (character === '{') {
+                nested++;
+                if (nested === 1) {
+                    skip = true;
+                    type = 'variable';
+                }
             }
 
-            if (currentType !== type) {
+            if (character === '}') {
+                nested--;
+                if (nested === 0) {
+                    skip = true;
+                    type = 'text';
+                }
+            }
+
+            if (character === '[') {
+                nested++;
+                if (nested === 1) {
+                    skip = true;
+                    type = 'optional';
+                }
+            }
+
+            if (character === ']') {
+                nested--;
+                if (nested === 0) {
+                    skip = true;
+                    type = 'text';
+                }
+            }
+
+            if (character === '(') {
+                nested++;
+                if (nested === 1) {
+                    skip = true;
+                    type = 'variational';
+                }
+            }
+
+            if (character === ')') {
+                nested--;
+                if (nested === 0) {
+                    skip = true;
+                    type = 'text';
+                }
+            }
+
+            if (
+                value.length > 0 &&
+                (currentType !== type || character === null)
+            ) {
+                const isBang = currentType === 'variable' && value.endsWith('!');
+                if (isBang) {
+                    value = value.slice(0, -1);
+                }
+
+                result.push({
+                    type: currentType,
+                    value: currentType === 'optional'
+                        ? this.build(value)
+                        : currentType === 'variational'
+                            ? value.split('|').map(v => this.build(v))
+                            : value,
+                    ...isBang && { bang: true },
+                });
                 value = '';
             }
 
+            currentType = type;
             if (!skip) {
                 value += character;
             }
 
             i++;
         }
+
+        return result;
     }
 }
 
--- test/PatternBuilder.test.js
+++ test/PatternBuilder.test.js
@@ -14,25 +14,156 @@ describe('PatternBuilder', () => {
     });
 
     describe('#build()', () => {
-        it.only('should build pattern from the input string (simple)', () => {
-            expect(patternBuilder.build('Buy {body}'))
-                .to.eq([
+        it('should parse simple text', () => {
+            expect(patternBuilder.build('Hello'))
+                .to.deep.eq([
+                    { type: 'text', value: 'hello' },
+                ]);
+
+            expect(patternBuilder.build('Buy chicken'))
+                .to.deep.eq([
+                    { type: 'text', value: 'buy chicken' },
+                ]);
+        });
+
+        it('should parse variables', () => {
+            expect(patternBuilder.build('{note}'))
+                .to.deep.eq([
+                    { type: 'variable', value: 'note' },
+                ]);
+
+            expect(patternBuilder.build('{tag}{note}'))
+                .to.deep.eq([
+                    { type: 'variable', value: 'tag' },
+                    { type: 'variable', value: 'note' },
+                ]);
+
+            expect(patternBuilder.build('{tag} {note}'))
+                .to.deep.eq([
+                    { type: 'variable', value: 'tag' },
+                    { type: 'text', value: ' ' },
+                    { type: 'variable', value: 'note' },
+                ]);
+
+            expect(patternBuilder.build('Buy {note}, please'))
+                .to.deep.eq([
                     { type: 'text', value: 'buy ' },
-                    { type: 'variable', value: 'body' },
+                    { type: 'variable', value: 'note' },
+                    { type: 'text', value: ', please' },
+                ]);
+
+            expect(patternBuilder.build('#{tag} Buy {note}, please'))
+                .to.deep.eq([
+                    { type: 'text', value: '#' },
+                    { type: 'variable', value: 'tag' },
+                    { type: 'text', value: ' buy ' },
+                    { type: 'variable', value: 'note' },
+                    { type: 'text', value: ', please' },
+                ]);
+        });
+
+        it('should parse bang variables', () => {
+            expect(patternBuilder.build('#{tag!} Buy {note}, please'))
+                .to.deep.eq([
+                    { type: 'text', value: '#' },
+                    { type: 'variable', value: 'tag', bang: true },
+                    { type: 'text', value: ' buy ' },
+                    { type: 'variable', value: 'note' },
+                    { type: 'text', value: ', please' },
+                ]);
+        });
+
+        it('should parse optionals', () => {
+            expect(patternBuilder.build('[hello]'))
+                .to.deep.eq([
+                    { type: 'optional', value: [{ type: 'text', value: 'hello' }] }
+                ]);
+
+            expect(patternBuilder.build('[hello][world]'))
+                .to.deep.eq([
+                    { type: 'optional', value: [{ type: 'text', value: 'hello' }] },
+                    { type: 'optional', value: [{ type: 'text', value: 'world' }] }
+                ]);
+
+            expect(patternBuilder.build('[hello] [world]'))
+                .to.deep.eq([
+                    { type: 'optional', value: [{ type: 'text', value: 'hello' }] },
+                    { type: 'text', value: ' ' },
+                    { type: 'optional', value: [{ type: 'text', value: 'world' }] }
+                ]);
+        });
+
+        it('should parse variations', () => {
+            expect(patternBuilder.build('(hello)'))
+                .to.deep.eq([
+                    {
+                        type: 'variational',
+                        value: [
+                            [{ type: 'text', value: 'hello' }],
+                        ]
+                    }
+                ]);
+
+            expect(patternBuilder.build('(hello|hi)'))
+                .to.deep.eq([
+                    {
+                        type: 'variational',
+                        value: [
+                            [{ type: 'text', value: 'hello' }],
+                            [{ type: 'text', value: 'hi' }],
+                        ]
+                    }
+                ]);
+
+            expect(patternBuilder.build('(hello|hi)(world|there)'))
+                .to.deep.eq([
+                    {
+                        type: 'variational',
+                        value: [
+                            [{ type: 'text', value: 'hello' }],
+                            [{ type: 'text', value: 'hi' }],
+                        ]
+                    },
+                    {
+                        type: 'variational',
+                        value: [
+                            [{ type: 'text', value: 'world' }],
+                            [{ type: 'text', value: 'there' }],
+                        ]
+                    }
+                ]);
+
+            expect(patternBuilder.build('(hello|hi) (world|there)'))
+                .to.deep.eq([
+                    {
+                        type: 'variational',
+                        value: [
+                            [{ type: 'text', value: 'hello' }],
+                            [{ type: 'text', value: 'hi' }],
+                        ]
+                    },
+                    { type: 'text', value: ' ' },
+                    {
+                        type: 'variational',
+                        value: [
+                            [{ type: 'text', value: 'world' }],
+                            [{ type: 'text', value: 'there' }],
+                        ]
+                    }
                 ]);
         });
 
         it('should build pattern from the input string (complex)', () => {
             expect(patternBuilder.build('(Buy|Purchase) {body}[,] please[ #{tag!}][(!|.)]'))
-                .to.eq([
+                .to.deep.eq([
                     {
                         type: 'variational',
                         value: [
-                            { type: 'text', value: 'buy' },
-                            { type: 'text', value: 'purchase' },
+                            [{ type: 'text', value: 'buy' }],
+                            [{ type: 'text', value: 'purchase' }],
                         ]
                     },
-                    { type: 'text', value: 'buy ' },
+                    { type: 'text', value: ' ' },
                     { type: 'variable', value: 'body' },
                     {
                         type: 'optional',
@@ -51,12 +182,48 @@ describe('PatternBuilder', () => {
                         value: [{
                             type: 'variational',
                             value: [
-                                { type: 'text', value: '!' },
-                                { type: 'text', value: '.' }
+                                [{ type: 'text', value: '!' }],
+                                [{ type: 'text', value: '.' }]
                             ]
                         }]
                     },
                 ]);
+        });
+
+        it('should parse nested cases', () => {
+            expect(patternBuilder.build('[[{tag}]]'))
+                .to.deep.eq([{
+                    "type": "optional",
+                    "value": [{
+                        "type": "optional",
+                        "value": [{
+                            "type": "variable",
+                            "value": "tag"
+                        }]
+                    }]
+                }]);
+
+            expect(patternBuilder.build('(([[hello]]))'))
+                .to.deep.eq([{
+                    "type": "variational",
+                    "value": [
+                        [{
+                            "type": "variational",
+                            "value": [
+                                [{
+                                    "type": "optional",
+                                    "value": [{
+                                        "type": "optional",
+                                        "value": [{
+                                            "type": "text",
+                                            "value": "hello"
+                                        }]
+                                    }]
+                                }]
+                            ]
+                        }]
+                    ]
+                }]);
         });
     });
 });
