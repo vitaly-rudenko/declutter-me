6df5a6662569a2952a806ad00662c060880e3c5e
Vitaly Rudenko <maurice.dederick@gmail.com>
2021-06-20 15:52:26 +0300

    Better rendering

--- frontend/src/template-builder/TemplateBuilder.js
+++ frontend/src/template-builder/TemplateBuilder.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {
     Container, TextField, Table, TableContainer, Paper, TableBody, TableHead, TableRow, TableCell, Chip, Link,
-    Card, CardContent, Typography
+    Card, CardContent, Typography, Button
 } from '@material-ui/core'
 import {
     TextFields, TextFormat, LocalOffer, LooksOne, Link as LinkIcon,
@@ -44,7 +44,8 @@ const fields = [
     new Field({ name: 'Tags', inputType: 'word', outputType: 'multi_select', value: ['man', 'young'] }),
     new Field({ name: 'URL', inputType: 'url', outputType: 'url', value: 'https://en.wikipedia.org/wiki/Jon_Snow_(character)' }),
     new Field({ name: 'E-mail', inputType: 'email', outputType: 'email', value: 'jon.snow@example.com' }),
-    // new Field({ name: 'Phone Number', inputType: 'phone', outputType: 'phone', value: '+1234567890' }),
+    new Field({ name: 'Phone Number', inputType: 'phone', outputType: 'phone', value: '+1234567890' }),
+    new Field({ name: 'Birthday', inputType: 'date', outputType: 'date', value: '1996.08.01 12:00' }),
 ];
 
 export const TemplateBuilder = () => {
@@ -52,10 +53,16 @@ export const TemplateBuilder = () => {
     const databaseFields = fields.filter(field => field.inputType !== 'database');
 
     return <div className="template-builder">
-        <Container maxWidth="lg">
+        <Container maxWidth="xl">
             <form className="template-builder__form" noValidate autoComplete="off">
-                <TextField label="Template" variant="outlined" fullWidth multiline value="{Name:text:text}: {Description:text:text} #{Type:word:select}[ #{Tags:word:multi_select}][ #{Tags:word:multi_select}][ {URL:url:url}]" />
-                <TextField label="Test" variant="outlined" fullWidth multiline value="Jon Snow: A character in the Game of Thrones #main #man #young https://en.wikipedia.org/wiki/Jon_Snow_(character)" />
+                <TextField
+                    label="Template" size="small" spellCheck={false} classes={{ root: 'template-builder__template-field' }} variant="outlined" fullWidth multiline
+                    value="{Name:text:text}: {Description:text:text} #{Type:word:select}[ #{Tags:word:multi_select}][ #{Tags:word:multi_select}][ {E-mail:email:email}][ {URL:url:url}]"
+                />
+                <TextField
+                    label="Test" size="small" spellCheck={false} variant="outlined" fullWidth multiline
+                    value="Jon Snow: A character in the Game of Thrones #main #man #young jon.snow@example.com https://en.wikipedia.org/wiki/Jon_Snow_(character)"
+                />
                 <Card>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
@@ -67,11 +74,23 @@ export const TemplateBuilder = () => {
                     </CardContent>
                 </Card>
                 <TableContainer component={Paper}>
-                    <Table size="small" aria-label="a dense table">
+                    <Table size="small">
                         <TableHead>
                             <TableRow>
                                 {databaseFields.map((field) => {
-                                    return <TableCell>{field.name}</TableCell>
+                                    const InputTypeIcon = InputTypeIcons[field.inputType];
+                                    const OutputTypeIcon = OutputTypeIcons[field.outputType];
+
+                                    if (!InputTypeIcon) {
+                                        throw new Error('Invalid input type: ' + field.inputType)
+                                    }
+
+                                    return <TableCell><Button disableRipple
+                                        classes={{
+                                            root: 'template-builder__field-name'
+                                        }}
+                                        startIcon={OutputTypeIcon ? <OutputTypeIcon/> : <InputTypeIcon/>}
+                                    >{field.name}</Button></TableCell>
                                 })}
                             </TableRow>
                         </TableHead>
@@ -84,7 +103,7 @@ export const TemplateBuilder = () => {
                                             : field.outputType === 'select'
                                                 ? <Chip label={field.value} />
                                                 : field.outputType === 'url'
-                                                    ? <><Link href={field.value} title={field.value}>{ellipsis(field.value, 30)}</Link></>
+                                                    ? <><Link href={field.value} title={field.value}>{ellipsis(field.value, 35)}</Link></>
                                                     : field.value
                                     )}</TableCell>
                                 })}
@@ -98,9 +117,9 @@ export const TemplateBuilder = () => {
 }
 
 function ellipsis(text, length) {
-    if (text.length <= length + 6) {
+    if (text.length <= length + 9) {
         return text;
     }
 
-    return text.slice(0, length) + '...' + text.slice(-3);
+    return text.slice(0, length) + '...' + text.slice(-6);
 }
