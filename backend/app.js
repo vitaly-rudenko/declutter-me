90c18868326770969982095e617b7df2b545a084
Vitaly Rudenko <maurice.dederick@gmail.com>
2021-07-23 00:43:51 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -794,11 +794,9 @@ function encodeTemplates(templates) {
         console.log('Domain:', domain, 'Port:', port, 'Bot token:', botToken);
 
         await bot.launch({
-            allowedUpdates: ['callback_query', 'message'],
             webhook: {
                 domain: process.env.DOMAIN,
                 port,
-                host: '0.0.0.0',
             }
         })
     } else {
