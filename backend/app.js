99164cf8390c01ab2196ea9ba5111e3e7e62e834
Vitaly Rudenko <maurice.dederick@gmail.com>
2021-09-05 11:51:06 +0300

    Moved API to a separate file

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,7 @@ import { versionCommand } from './app/flows/version.js';
 import { apiCommand } from './app/flows/api.js';
 import { match } from './app/match.js';
 import { NotionAccountNotFound } from './app/errors/NotionAccountNotFound.js';
+import { sendMessage } from './app/api/messages.js';
 
 const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN;
 
@@ -187,72 +188,7 @@ const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN;
         }
     });
 
-    app.post('/api/messages', async (req, res, next) => {
-        try {
-            const apiKey = req.get('api-key')
-            if (!apiKey) {
-                return res.status(422).json({ error: { code: 'API_KEY_NOT_PROVIDED' } });
-            }
-
-            const text = req.body.text
-            if (!text) {
-                return res.status(422).json({ error: { code: 'TEXT_NOT_PROVIDED' } });
-            }
-    
-            const user = await storage.findUserByApiKey(apiKey);
-            if (!user) {
-                return res.status(401).json({ error: { code: 'API_KEY_NOT_FOUND' } });
-            }
-    
-            let notion;
-            try {
-                ([notion] = await notionSessionManager.get(user.id));
-            } catch (error) {
-                if (error instanceof NotionAccountNotFound) {
-                    return res.status(400).json({ error: { code: 'NOTION_ACCOUNT_NOT_CONFIGURED' } });
-                }
-
-                throw error;
-            }
-    
-            const result = await match({
-                user,
-                notion,
-                storage,
-                text,
-            });
-    
-            if (!result.match) {
-                return res.status(404).json({ match: null });
-            }
-    
-            res.json({
-                match: {
-                    fields: result.match.fields.map(field => ({
-                        name: field.name,
-                        inputType: field.inputType,
-                        value: field.value,
-                    })),
-                    database: {
-                        alias: result.match.database.alias,
-                        notionDatabaseUrl: result.match.database.notionDatabaseUrl,
-                    },
-                    notionPage: {
-                        id: result.match.notion.pageId,
-                        url: result.match.notion.pageUrl,
-                    },
-                    combination: result.match.combination,
-                }
-            });
-        } catch (error) {
-            res.status(500).json({
-                error: {
-                    code: 'UNEXPECTED_ERROR',
-                    message: error.message,
-                }
-            });
-        }
-    })
+    app.post('/api/messages', sendMessage({ notionSessionManager, storage }))
 
     await new Promise(resolve => app.listen(port, () => resolve()));
 
