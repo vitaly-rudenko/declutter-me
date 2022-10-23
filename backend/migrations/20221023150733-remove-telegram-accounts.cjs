bd315589a28b1f5425181f6e00134b5e21b7c4c3
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-10-23 18:21:20 +0300

    fix: migration

--- backend/migrations/20221023150733-remove-telegram-accounts.cjs
+++ backend/migrations/20221023150733-remove-telegram-accounts.cjs
@@ -3,7 +3,7 @@ module.exports = {
     up: async (db) => {
         await db.query(`
             BEGIN;
-            UPDATE users SET id = (SELECT telegram_user_id FROM telegram_accounts WHERE user_id = user.id);
+            UPDATE users SET id = (SELECT telegram_user_id FROM telegram_accounts WHERE user_id = users.id);
             DROP TABLE telegram_accounts;
             COMMIT;
         `)
