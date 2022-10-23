ac7fef024362a10df652c36e8556f0c2d920250b
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-10-23 18:26:05 +0300

    fix: use escapeMd from telegramify

--- backend/app/utils/escapeMd.js
+++
@@ -1,7 +0,0 @@
-import { markdownEscapes } from 'markdown-escapes';
-
-const ESCAPE_REGEX = new RegExp(`(?<!\\\\)([\\${markdownEscapes.join('\\')}])`, 'g');
-
-export const escapeMd = (string) =>  {
-    return string.replace(ESCAPE_REGEX, '\\$1');
-}
