3e4bf27065c0bd09ab26271e1e78d0abd6e66b76
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-11-30 01:43:54 +0200

    Bootstrapped rudimentary implementation of applyMarkdownModification()

---
+++ obsidian-plugin/src/markdown/apply-markdown-modification.test.ts
@@ -0,0 +1,177 @@
+import { applyMarkdownModification } from './apply-markdown-modification'
+
+describe('applyMarkdownModification()', () => {
+    describe('type: appendLineAfterContent', () => {
+        function testBasic(options: Omit<Parameters<typeof applyMarkdownModification>[0], 'markdown' | 'content'>) {
+            expect(applyMarkdownModification({
+                ...options,
+                markdown: '',
+                content: 'test'
+            })).toBe('test')
+
+            expect(applyMarkdownModification({
+                ...options,
+                markdown: 'hello world',
+                content: 'test',
+            })).toBe('hello world\ntest')
+
+            expect(applyMarkdownModification({
+                ...options,
+                markdown: '\n\n\nhello world\n\n\n',
+                content: 'test',
+            })).toBe('\n\n\nhello world\ntest\n\n\n')
+
+            expect(applyMarkdownModification({
+                ...options,
+                markdown: '\n\n\nhello world\n\n\nhello world\nhi there\n\n\n',
+                content: 'multiline\ntest',
+            })).toBe('\n\n\nhello world\n\n\nhello world\nhi there\nmultiline\ntest\n\n\n')
+        }
+
+        it('defaults', () => {
+            testBasic({
+                type: 'appendLineAfterContent',
+                skipProperties: false,
+            })
+        })
+
+        it('skipProperties: true', () => {
+            testBasic({
+                type: 'appendLineAfterContent',
+                skipProperties: true,
+            })
+
+            const options = {
+                type: 'appendLineAfterContent',
+                skipProperties: true,
+                content: 'test'
+            } as const
+
+            expect(applyMarkdownModification({
+                markdown: '---\nhello: world\n---',
+                ...options,
+            })).toBe('---\nhello: world\n---\ntest')
+
+            expect(applyMarkdownModification({
+                markdown: '---\nhello: world\n---\nhello world',
+                ...options,
+            })).toBe('---\nhello: world\n---\nhello world\ntest')
+
+            expect(applyMarkdownModification({
+                markdown: '---\nhello: world\n---\n\n---\n\nhello world\n\n\n',
+                ...options,
+            })).toBe('---\nhello: world\n---\n\n---\n\nhello world\ntest\n\n\n')
+        })
+
+        it('section: "# section"', () => {
+            const options = {
+                type: 'appendLineAfterContent',
+                skipProperties: false,
+                section: '# section',
+                content: 'test',
+            } as const
+
+            expect(applyMarkdownModification({
+                markdown: '',
+                ...options,
+            })).toBe('# section\n\ntest')
+
+            expect(applyMarkdownModification({
+                markdown: '# section\n\nhello world',
+                ...options,
+            })).toBe('# section\n\nhello world\ntest')
+
+            expect(applyMarkdownModification({
+                markdown: '# section before\n# section\n#section after\n',
+                ...options,
+            })).toBe('# section before\n# section\ntest\n#section after\n')
+        })
+    })
+
+    describe('type: prependLineBeforeContent', () => {
+        function testBasic(options: Omit<Parameters<typeof applyMarkdownModification>[0], 'markdown' | 'content'>) {
+            expect(applyMarkdownModification({
+                ...options,
+                markdown: '',
+                content: 'test'
+            })).toBe('test')
+
+            expect(applyMarkdownModification({
+                ...options,
+                markdown: 'hello world',
+                content: 'test',
+            })).toBe('test\nhello world')
+
+            expect(applyMarkdownModification({
+                ...options,
+                markdown: '\n\n\nhello world\n\n\n',
+                content: 'test',
+            })).toBe('\n\n\ntest\nhello world\n\n\n')
+
+            expect(applyMarkdownModification({
+                ...options,
+                markdown: '\n\n\nhello world\n\n\nhello world\nhi there\n\n\n',
+                content: 'multiline\ntest',
+            })).toBe('\n\n\nmultiline\ntest\nhello world\n\n\nhello world\nhi there\n\n\n')
+        }
+
+        it('defaults', () => {
+            testBasic({
+                type: 'prependLineBeforeContent',
+                skipProperties: false,
+            })
+        })
+
+        it('skipProperties: true', () => {
+            testBasic({
+                type: 'prependLineBeforeContent',
+                skipProperties: true,
+            })
+
+            const options = {
+                type: 'prependLineBeforeContent',
+                skipProperties: true,
+                content: 'test'
+            } as const
+
+            expect(applyMarkdownModification({
+                markdown: '---\nhello: world\n---',
+                ...options,
+            })).toBe('---\nhello: world\n---\ntest')
+
+            expect(applyMarkdownModification({
+                markdown: '---\nhello: world\n---\nhello world',
+                ...options,
+            })).toBe('---\nhello: world\n---\ntest\nhello world')
+
+            expect(applyMarkdownModification({
+                markdown: '---\nhello: world\n---\n\n---\n\nhello world\n\n\n',
+                ...options,
+            })).toBe('---\nhello: world\n---\n\ntest\n---\n\nhello world\n\n\n')
+        })
+
+        it('section: "# section"', () => {
+            const options = {
+                type: 'prependLineBeforeContent',
+                skipProperties: false,
+                section: '# section',
+                content: 'test',
+            } as const
+
+            expect(applyMarkdownModification({
+                markdown: '',
+                ...options,
+            })).toBe('# section\n\ntest')
+
+            expect(applyMarkdownModification({
+                markdown: '# section\n\nhello world',
+                ...options,
+            })).toBe('# section\n\ntest\nhello world')
+
+            expect(applyMarkdownModification({
+                markdown: '# section before\n# section\n#section after\n',
+                ...options,
+            })).toBe('# section before\n# section\ntest\n#section after\n')
+        })
+    })
+})
---
+++ obsidian-plugin/src/markdown/apply-markdown-modification.ts
@@ -0,0 +1,58 @@
+export function applyMarkdownModification(input: {
+    markdown: string,
+    type: 'appendLineAfterContent' | 'prependLineBeforeContent'
+    section?: string
+    skipProperties: boolean
+    content: string
+}) {
+    const { markdown, type, section, skipProperties, content } = input
+
+    const lines = markdown.split('\n')
+
+    let startIndex = 0
+    let endIndex = lines.length
+
+    if (section) {
+        startIndex = lines.indexOf(section, startIndex)
+        if (startIndex === -1) {
+            const lastNonEmptyLineIndex = Math.max(0, lines.findLastIndex(line => line.trim() !== ''))
+            lines.splice(lastNonEmptyLineIndex, 0, section)
+            startIndex = lastNonEmptyLineIndex + 2
+        }
+
+        endIndex = lines.findIndex((line, index) => index > startIndex && line.startsWith('#'))
+        if (endIndex === -1) {
+            endIndex = lines.length
+        }
+    } else if (skipProperties) {
+        if (lines[0] === '---') {
+            const propertiesEndIndex = lines.indexOf('---', 1)
+            if (propertiesEndIndex !== -1) {
+                startIndex = propertiesEndIndex + 1
+            }
+        }
+    }
+
+    if (type === 'appendLineAfterContent') {
+        // @ts-ignore
+        const lastNonEmptyLineIndex = lines.findLastIndex((line, index) => index >= startIndex && index < endIndex && line.trim() !== '')
+        if (lastNonEmptyLineIndex === -1) {
+            lines[startIndex] = content
+        } else {
+            lines.splice(lastNonEmptyLineIndex + 1, 0, content)
+        }
+    } else if (type === 'prependLineBeforeContent') {
+        const firstNonEmptyLineIndex = lines.findIndex((line, index) => index > startIndex && index <= endIndex && line.trim() !== '')
+        if (firstNonEmptyLineIndex === -1) {
+            if (lines[startIndex] !== undefined && lines[startIndex].trim() !== '') {
+                lines.splice(startIndex, 0, content)
+            } else {
+                lines[startIndex] = content
+            }
+        } else {
+            lines.splice(firstNonEmptyLineIndex, 0, content)
+        }
+    }
+
+    return lines.join('\n')
+}
