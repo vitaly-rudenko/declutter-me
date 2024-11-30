39ac553c83d53e1f801ca08b2f64e9bcde84a27e
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-11-30 11:31:42 +0200

    Added test for replaceVariables()

--- obsidian-plugin/src/replace-variables.test.ts
+++ obsidian-plugin/src/replace-variables.test.ts
@@ -1,1 +1,15 @@
-// TODO
+import { replaceVariables } from "./replace-variables"
+
+describe('replaceVariables()', () => {
+  it('replaces variables', () => {
+    expect(replaceVariables(
+      'Hello, {name} {surname}! Your age is {age}.',
+      { name: 'John', surname: 'Doe', age: 30 }
+    )).toBe('Hello, John Doe!')
+  })
+
+  it('replaces dates', () => {
+    const now = new Date(2021, 0, 1)
+    expect(replaceVariables('Today is {date:yyyy-MM-dd}. Year: {date:yyyy}!', {}, now)).toBe('Today is 2021-01-01. Year: 2021!')
+  })
+})
--- obsidian-plugin/src/replace-variables.ts
+++ obsidian-plugin/src/replace-variables.ts
@@ -1,12 +1,12 @@
 import { format } from 'date-fns'
 
-export function replaceVariables(input: string, variables: Record<string, string>) {
+export function replaceVariables(input: string, variables: Record<string, string | number>, now = new Date()) {
   let result = input
   for (const [name, value] of Object.entries(variables)) {
     const variableName = '{' + name + '}'
     while (result.includes(variableName)) {
-      result = result.replace(variableName, value)
+      result = result.replace(variableName, String(value))
     }
   }
-  return result.replace(/\{date:(.+?)\}/i, (_, dateFormat) => format(new Date(), dateFormat))
+  return result.replace(/\{date:(.+?)\}/ig, (_, dateFormat) => format(now, dateFormat))
 }
