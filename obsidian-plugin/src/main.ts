546a01d97fb86c0222ad018837d23bf940035bdf
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-11-30 13:04:20 +0200

    Custom variables, device name, reset button

--- obsidian-plugin/src/main.ts
+++ obsidian-plugin/src/main.ts
@@ -16,11 +16,15 @@ const routeSchema = z.object({
 	templatePath: z.string().optional(),
 })
 
-type Variables = Record<string, string | number>
+const variablesSchema = z.record(z.union([z.number(), z.string()]))
+
+type Variables = z.infer<typeof variablesSchema>
 type Route = z.infer<typeof routeSchema>
 
 type DeclutterMePluginSettings = {
 	routes: Route[]
+	device: string
+	variables: Variables
 }
 
 const routesSchema = z.array(routeSchema)
@@ -46,28 +50,34 @@ const routesExample: Route[] = [
 	}
 ]
 
+const variablesExample: Variables = {
+	hello: 'world'
+}
+
 const DEFAULT_SETTINGS: DeclutterMePluginSettings = {
-	// routes: [],
 	routes: routesExample,
+	device: 'Personal Macbook',
+	variables: variablesExample,
 }
 
 export class ExampleModal extends SuggestModal<Route> {
   private latestQuery: string | undefined;
 
-  constructor(app: App, private readonly onTrigger: (query: string) => void) {
+  constructor(app: App, private readonly settings: DeclutterMePluginSettings, private readonly onTrigger: (query: string) => void) {
 	super(app)
   }
 
   getSuggestions(query: string): Route[] {
     this.latestQuery = query
-    const matchedRoute = routesExample.find((route) => match(query, route.template) !== undefined);
-	return matchedRoute ? [matchedRoute] : routesExample
+    const matchedRoute = this.settings.routes.find((route) => match(query, route.template) !== undefined);
+	return matchedRoute ? [matchedRoute] : this.settings.routes
   }
 
   renderSuggestion(route: Route, el: HTMLElement) {
     const matchResult = this.latestQuery ? match(this.latestQuery, route.template) : undefined
 	const variables: Variables = {
-		device: 'Personal Macbook',
+		device: this.settings.device,
+		...this.settings.variables,
 		...matchResult ? transformMatchResultToVariables(matchResult) : {},
 	}
 
@@ -98,10 +108,14 @@ export default class DeclutterMePlugin extends Plugin {
 			matchResult = match(input, route.template)
 			if (matchResult) break
 		}
-		if (!matchedRoute || !matchResult) return // TODO: warning
+		if (!matchedRoute || !matchResult) {
+			new Notice('Could not parse input')
+			return
+		}
 
 		const variables: Variables = {
-			device: 'Personal Macbook',
+			device: this.settings.device,
+			...this.settings.variables,
 			...inputVariables,
 			...transformMatchResultToVariables(matchResult),
 		}
@@ -161,7 +175,7 @@ export default class DeclutterMePlugin extends Plugin {
 			id: 'declutter-me-spotlight',
 			name: 'Spotlight',
 			callback: () => {
-				new ExampleModal(this.app, async (query) => {
+				new ExampleModal(this.app, this.settings, async (query) => {
 					await this.handleQuery(query, {})
 				}).open()
 			}
@@ -177,12 +191,25 @@ export default class DeclutterMePlugin extends Plugin {
 	onunload() {}
 
 	async loadSettings() {
-		// this.settings = { ...DEFAULT_SETTINGS, ...await this.loadData() }
-		this.settings = { ...DEFAULT_SETTINGS }
+		let variables: Variables = {}
+		try {
+			variables = variablesSchema.parse(JSON.parse(this.app.loadLocalStorage('declutter-me:variables') as string))
+		} catch {
+			new Notice('Could not load variables')
+		}
+
+		this.settings = {
+			...DEFAULT_SETTINGS,
+			...await this.loadData(),
+			device: String(this.app.loadLocalStorage('declutter-me:device') || ''),
+			variables,
+		}
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings)
+		await this.app.saveLocalStorage('declutter-me:device', this.settings.device)
+		await this.app.saveLocalStorage('declutter-me:variables', JSON.stringify(this.settings.variables))
 	}
 }
 
@@ -199,8 +226,43 @@ class SampleSettingTab extends PluginSettingTab {
 		this.containerEl.createEl('h2', { text: this.plugin.manifest.name })
 
 		new Setting(this.containerEl)
+			.setName('Device name')
+			.setDesc('Can be used as {device} in paths and contents. Not synced to other devices.')
+			.addText((text) => {
+				return text
+					.setPlaceholder(DEFAULT_SETTINGS.device)
+					.setValue(this.plugin.settings.device)
+					.onChange(async (value) => {
+						this.plugin.settings.device = value
+
+						await this.plugin.saveSettings()
+					})
+			})
+
+		new Setting(this.containerEl)
+			.setName('Variables')
+			.setDesc('In JSON format. Can be used as {variable} in paths and contents. Not synced to other devices.')
+			.addTextArea((textArea) => {
+				textArea.inputEl.rows = 10
+				textArea.inputEl.cols = 60
+
+				return textArea
+					.setPlaceholder(JSON.stringify(variablesExample, null, 2))
+					.setValue(JSON.stringify(this.plugin.settings.variables, null, 2))
+					.onChange(async (value) => {
+						try {
+							this.plugin.settings.variables = variablesSchema.parse(JSON.parse(value))
+						} catch {
+							new Notice('Could not parse variables')
+						}
+
+						await this.plugin.saveSettings()
+					})
+			})
+
+		new Setting(this.containerEl)
 			.setName('Routes')
-			.setDesc('In JSON format')
+			.setDesc('In JSON format. Synced to all devices.')
 			.addTextArea((textArea) => {
 				textArea.inputEl.rows = 20
 				textArea.inputEl.cols = 60
@@ -211,9 +273,27 @@ class SampleSettingTab extends PluginSettingTab {
 					.onChange(async (value) => {
 						try {
 							this.plugin.settings.routes = routesSchema.parse(JSON.parse(value))
-						} catch { /* ignore */ }
+						} catch {
+							new Notice('Could not parse routes')
+						}
 
 						await this.plugin.saveSettings()
+					})
+			})
+
+		new Setting(this.containerEl)
+			.setName('Danger zone')
+			.addButton((button) => {
+				return button
+					.setButtonText('Reset routes')
+					.setWarning()
+					.onClick(async () => {
+						this.plugin.settings.routes = [...routesExample]
+
+						await this.plugin.saveSettings()
+						this.display()
+
+						new Notice('Routes have been reset')
 					})
 			})
 	}
